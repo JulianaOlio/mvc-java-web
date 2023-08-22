29ca8bbd744d3e742f5d8a29622306751b2a16f2
jully <jullymary@hotmail.com>
2023-08-21 21:10:45 -0300

    atualiz Hello World Maven + JSP

---
+++ aulas/aula9-16-08-2023/HelloWorld/src/main/java/model/HelloWorld.java
@@ -0,0 +1,22 @@
+package model;
+
+public class HelloWorld {
+
+	
+	String frases = " ";
+
+	public HelloWorld(String frases) {
+		this.frases = frases;
+	}
+
+	public String getFrases() {
+		return frases;
+	}
+
+	public void setFrases(String frases) {
+		this.frases = frases;
+	}
+	
+	
+	
+}
